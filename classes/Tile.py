626e4ad60ccc543a31964566b090cba0bec3087d
leoboehm <boeleo425@gmail.com>
2024-01-30 18:02:28 +0100

    WIP: #5 added color scheme to tile class

--- classes/Tile.py
+++ classes/Tile.py
@@ -1,18 +1,28 @@
 import random
 
 class Tile:
-    width: 10
-    height: 10
-
-    posX: 0
-    posY: 0
-
     value: 0
 
-    def __init__(self):
-        # set position
+    colordict = {
+        0: (0,0,0), # black
+        2: (255,0,0), # red
+        4: (0,128,0), # green
+        8: (156,39,176), # purple
+        16: (103,58,183), # deep purple
+        32: (255,87,34), # deep orange
+        64: (0,150,136), # teal
+        128: (139,195,74), # light green
+        256: (234,30,99), # pink
+        512: (255,152,0), # orange
+        1024: (33,150,136), # blue
+        2048: (121,85,72) # brown
+    }
 
+    def __init__(self):
         self.value = random.choice([2, 4])
+
+    def getColor(self):
+        return self.colordict[self.value]
 
     def doubleValue(self):
         self.value = self.value * 2

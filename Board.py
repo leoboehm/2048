6f4c877589f263da330b73104b74ac2be24d9385
leoboehm <boeleo425@gmail.com>
2024-02-05 15:46:56 +0100

    WIP: #8 implemented tile value to board matrix

--- Board.py
+++ Board.py
@@ -6,6 +6,21 @@ class Board:
 
     grid = []
     
+    colordict = {
+        0: (255,255,255), # white
+        2: (255,0,0), # red
+        4: (0,128,0), # green
+        8: (156,39,176), # purple
+        16: (103,58,183), # deep purple
+        32: (255,87,34), # deep orange
+        64: (0,150,136), # teal
+        128: (139,195,74), # light green
+        256: (234,30,99), # pink
+        512: (255,152,0), # orange
+        1024: (33,150,136), # blue
+        2048: (121,85,72) # brown
+    }
+    
     def __init__(self, display):
         self._display = display
         self.draw_grid()
@@ -18,7 +33,7 @@ class Board:
         #create a 2 dimensional array
         for row in range(4):
             self.grid.append([])
-            for column in range(4):
+            for col in range(4):
                 self.grid[row].append(0)
         clock = pygame.time.Clock()
 
@@ -29,26 +44,28 @@ class Board:
             for event in pygame.event.get(): #User did something
                 if event.type == pygame.QUIT: #If user clicked close
                     done = True      #exit this loop  
+                
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     #user clicks the mouse + get the position
                     pos = pygame.mouse.get_pos()
                     #change the x/y screen coordinates to self.grid coordinates
-                    column = pos[0] //(width + margin)
+                    col = pos[0] //(width + margin)
                     row = pos[1] // (height + margin)
 
-                    #Set location to one
-                    self.grid[row][column] = 1
-                    print("Click", pos, "self.grid coordinates: ", row, column)
+                    # double value
+                    if self.grid[row][col] > 0:
+                        self.grid[row][col] = self.doubleValue(self.grid[row][col])
+                    else: self.grid[row][col] = 2
+
+                    print("Click", pos, "self.grid coordinates: ", row, col, "Grid: ", self.grid)
            
 
             for row in range(4):
-                for column in range(4):
-                    color = (255, 255, 255)
-                    if self.grid[row][column] == 1:
-                        color = (255, 0, 0)
+                for col in range(4):
+                    color = self.getTileColor(self.grid[row][col])
                     pygame.draw.rect(self._display,
                                     color,
-                                    [(margin + width)* column + margin,
+                                    [(margin + width)* col + margin,
                                     (margin + height)* row + margin,
                                     width,
                                     height])
@@ -57,7 +74,26 @@ class Board:
 
             pygame.display.flip()
 
+    def canMove(self):
+        # check whether tile movement is possible
+        return True
 
+    # def spawnTile(self):
+    #     # spawn single tile on random free position
+        
+    def getTileColor(self, value):
+        return self.colordict[value]
+    
+    def doubleValue(self, value):
+        return value * 2
+    
+    # def moveTiles(self):
+    #     # move tiles and save new coordinates
+    
+    # def mergeTiles(self):
+    #     # merge tiles on movement if their values are identical
 
-
+    def sumScore(self):
+        # calculate score
+        return 0
         
